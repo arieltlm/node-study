76b39421f920ebd6b49d47b79f57cb971ad9ccfa
“arieltlm” <695353064@qq.com>
2021-01-25 23:10:54 +0800

    fix

--- node-start-imooc/express-practice/view/example/credit/index.tsx
+++ node-start-imooc/express-practice/view/example/credit/index.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
-import { Modal } from 'antd'
+import { Modal,Form,Input,DatePicker } from 'antd'
 import {IListItem} from '../lists'
 
+const {TextArea} = Input
 interface ICreditProps {
     editRowData?: IListItem | {}
     visible:boolean
     handleCancel:()=> void
 }
- 
+
+const formItemLayout = {
+    labelCol: {
+      xs: { span: 24 },
+      sm: { span: 6 },
+    },
+    wrapperCol: {
+      xs: { span: 24 },
+      sm: { span: 18 },
+    },
+  };
 
 const Credit: React.FC<ICreditProps> = ({
     editRowData,
@@ -15,12 +26,32 @@ const Credit: React.FC<ICreditProps> = ({
     handleCancel
 }) => {
     console.log(editRowData)
+
+    const [form] = Form.useForm();
+
+
+    const handleOk = () => {
+        form.validateFields().then(values => {
+            console.log(values)
+        }).catch(errorInfo => {
+            console.log('err',errorInfo)
+        })
+    }
+
     return <Modal 
         title="新建修改"
         visible={visible}
         onCancel={handleCancel}
+        onOk={handleOk}
         >
-        新建修改
+        <Form {...formItemLayout} form={form}>
+            <Form.Item name="deadline" label="截止日期" rules={[{required:true,message:'请选择日期'}]}>
+                <DatePicker style={{width:'100%'}} placeholder="请选择截止日期"/>
+            </Form.Item>
+            <Form.Item name="content" label="内容" rules={[{required:true,message:'请填写内容'}]} >
+                <TextArea rows={4} />
+            </Form.Item>
+        </Form>
     </Modal>
 }
 
